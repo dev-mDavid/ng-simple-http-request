ce693f05f51ec499c72a1fb056f4c1a378f691b4
mDavid <dev.mdavid@gmail.com>
2019-12-31 13:34:45 -0800

    generate Simple HTTP Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleHTTPComponent } from './simple-http/simple-http.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHTTPComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/simple-http/simple-http.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-http',
+  templateUrl: './simple-http.component.html',
+  styleUrls: ['./simple-http.component.sass']
+})
+export class SimpleHTTPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
